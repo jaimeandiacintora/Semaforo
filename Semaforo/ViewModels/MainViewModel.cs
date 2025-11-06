28b115f1677aec2ffc8ed2d1e529d63d4a89b0f5
jac17 <andiacintorajaime@gmail.com>
2025-11-06 09:08:27 +0100

    Semaforo

--- Semaforo/ViewModels/MainViewModel.cs
+++ Semaforo/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,66 @@ namespace Semaforo.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
+        
+        
+        private readonly Color _colorEncendidoRojo=Colors.Red;
+        private readonly Color _colorEncendidoAmarillo=Colors.Yellow;
+        private readonly Color _colorEncendidoVerde=Colors.Green;
+        private readonly Color _colorDefecto = Colors.Black;
+
+        [ObservableProperty]
+        private Color _colorSemaforo1;
+        [ObservableProperty]
+        private Color _colorSemaforo2;
+        [ObservableProperty]
+        private Color _colorSemaforo3;
+
+        #region CONSTRUCTOR 
         public MainViewModel()
         {
+            _colorSemaforo1 = _colorEncendidoRojo;
+            _colorSemaforo2 = _colorEncendidoAmarillo;
+            _colorSemaforo3 = _colorEncendidoVerde;
         }
+        #endregion
 
-
+        #region COMANDS
+        [RelayCommand]
+        private void encenderSemaforo1()
+        {
+            if (ColorSemaforo1 == _colorDefecto)
+            {
+                ColorSemaforo1 = _colorEncendidoRojo;
+            }
+            else
+            {
+                ColorSemaforo1 = _colorDefecto;
+            }   
+        }
+        [RelayCommand]
+        private void encenderSemaforo2()
+        {
+            if (ColorSemaforo2 == _colorDefecto)
+            {
+                ColorSemaforo2 = _colorEncendidoAmarillo;
+            }
+            else
+            {
+                ColorSemaforo2 = _colorDefecto;
+            }
+        }
+        [RelayCommand]
+        private void encenderSemaforo3()
+        {
+            if (ColorSemaforo3 == _colorDefecto)
+            {
+                ColorSemaforo3 = _colorEncendidoVerde;
+            }
+            else
+            {
+                ColorSemaforo3 = _colorDefecto;
+            }
+        }
+        #endregion
     }
 }
